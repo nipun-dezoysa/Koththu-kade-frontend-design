d9549158da59bcffa882ca32c5c016ac107cc95a
Nipun Avishka De Zoysa <nipunavishka123@gmail.com>
2023-06-17 23:00:48 +0530

    cart frontend update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 let a;
 let c = true;
+let l = false;
+let price =0;
 function oii(b){
     const one = document.querySelector('#one');
     const two = document.querySelector('#two');
@@ -20,11 +22,42 @@ function oii(b){
         case 5:five.className = 'sitem';break;
     }
     a=b;
+    l=true;
     }
     
 }
 function sub(){
+   if(l){
     c=false;
     document.getElementById("submits").value = "SUBMITED";
+    document.getElementById("rating-h1").innerHTML = "Thank you!";
+    document.getElementById("rating-p").innerHTML = "We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!";
     document.getElementById("submits").className = "selected";
+   }
+}
+function test(g,p){
+    const f = document.getElementById(g);
+    if(f.value=="Add to cart"){
+        f.value = "Added";
+        f.className = "addselect";
+        price+=p;
+    }else{
+        price-=p;
+        f.value = "Add to cart";
+        f.className = "test";
+    }
+    document.getElementById("row-item-price").innerHTML = "LKR "+price+".00";
+}
+
+function change(c,d,p){
+    if(document.getElementById(c).className=="sub-item"){
+        document.getElementById(c).className= "sub-item-select";
+        document.getElementById(d).className= "fa-solid fa-circle-minus fa-sm";
+        price+=p;
+    }else{
+        price-=p;
+        document.getElementById(c).className= "sub-item";
+        document.getElementById(d).className= "fa-solid fa-circle-plus fa-sm";
+    }
+    document.getElementById("row-item-price").innerHTML = "LKR "+price+".00";
 }
